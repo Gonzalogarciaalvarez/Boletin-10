65bbe31398441575b637544ffe33cc4d3e954625
ggarciaalvarez <ggarciaalvarez@atanasoff11.danielcastelao.org>
2015-11-19 11:08:17 +0100

    Commit para subir

---
+++ Boletin 10.2/src/boletin/pkg10/pkg2/Boletin102.java
@@ -0,0 +1,13 @@
+package boletin.pkg10.pkg2;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Boletin102 {
+
+    public static void main(String[] args) {
+        Metodos obx = new Metodos();
+        obx.numeroAletorio();
+    }
+}
